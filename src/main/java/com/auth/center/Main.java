c6c1f1af85787ea924447edf1337a2d1693857ed
zhanghongjie <zhanghj@xinfushe.com>
2018-01-18 17:16:48 +0800

    提交文件夹-2018年1月18日

---
+++ src/main/java/com/auth/center/Main.java
@@ -0,0 +1,12 @@
+package com.auth.center;
+
+/**
+ * @author
+ * @create 2018-01-18 17:13
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
